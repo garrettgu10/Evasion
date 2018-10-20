7109c56387b23e9ee489d3811569386ee885bf8f
Garrett Gu <garrettgu777@gmail.com>
2018-10-20 13:49:39 -0500

    added player display

--- public/javascripts/maze_display.js
+++ public/javascripts/maze_display.js
@@ -9,11 +9,27 @@ const TEST_ARRAY = [
     [0, 2, 1, 2, 0],
     [0, 0, 0, 0, 0]
 ]
+const TEST_PLAYERS = [
+    {
+        x: 50,
+        y: 50,
+        color: "#ff0000"
+    },
+    {
+        x: 25, 
+        y: 25,
+        color: "#00ff00"
+    }
+]
+
+const PLAYER_MAX_X = 100;
+const PLAYER_MAX_Y = 100;
+const PLAYER_RADIUS = 10;
 
 const GRAY_COLOR = "#BFBFBF";
 const BLACK_COLOR = "#000000";
 
-function draw_maze(context, maze){
+function draw_maze(context, maze, players = []){
     var width = maze.length;
     var height = maze[0].length;
     var canvasHeight = canvas.height;
@@ -34,12 +50,9 @@ function draw_maze(context, maze){
             }
             context.strokeStyle= (maze[x][y] === GRAY) ? GRAY_COLOR : BLACK_COLOR;
             
-            
             var lineX = Math.floor(x/2) * blockWidth;
             var lineStartY = Math.floor((y-1)/2) * blockHeight;
             var lineEndY = lineStartY + blockHeight;
-
-            console.log(lineX, lineStartY, lineEndY);
 
             context.beginPath();
             context.moveTo(lineX, lineStartY);
@@ -60,11 +73,10 @@ function draw_maze(context, maze){
                 continue;
             }
             context.strokeStyle= (maze[x][y] === GRAY) ? GRAY_COLOR : BLACK_COLOR;
-            
+
             var lineY = Math.floor((y)/2) * blockHeight;
             var lineStartX = Math.floor((x - 1)/2) * blockWidth;
             var lineEndX = lineStartX + blockHeight;
-
 
             context.beginPath();
             context.moveTo(lineStartX, lineY);
@@ -74,10 +86,25 @@ function draw_maze(context, maze){
             context.stroke();
         }
     }
+
+    //draw players
+    for(var player of players) {
+        var x = player.x / PLAYER_MAX_X * canvasWidth;
+        var y = player.y / PLAYER_MAX_Y * canvasHeight;
+        
+        var rad = PLAYER_RADIUS;
+        var color = player.color || "#000000";
+
+        context.beginPath();
+        context.fillStyle = color;
+        context.arc(x, y, rad, 0, 2*Math.PI);
+        context.fill();
+        
+    }
 }
 
 
 console.log('loaded');
 var canvas = document.getElementById("maze_canvas");
 console.log(canvas);
-draw_maze(canvas.getContext('2d'), TEST_ARRAY);
+draw_maze(canvas.getContext('2d'), TEST_ARRAY, TEST_PLAYERS);
