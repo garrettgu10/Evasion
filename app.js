c8801e37d143da914658823abfe2e55a539dfc10
aditya-arjun <aditya.arjun123@gmail.com>
2018-10-20 16:42:51 -0500

    Added server work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var server = require('./server/server');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+server(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/server.js
+++ server/server.js
@@ -1,4 +1,76 @@
-const MAX_SPEED = 0.5;
-const MIN_SPEED = -0.5;
+const MAX_SPEED = 1;
+const MIN_SPEED = -1;
 const UPDATE_RATE = 100; //Milliseconds
+const MAZE_SIZE = 101;
+const EMPTY = 2;
 
+var mazeGen = require('./maze/RecursiveMazeGenerator');
+var maze = mazeGen(MAZE_SIZE, MAZE_SIZE);
+var players = {};
+
+function setup(app) {
+    app.use('/new_game', function(req, res) {
+        maze = mazeGen(MAZE_SIZE, MAX_SIZE);
+        players = {};
+    });
+
+    app.use('/get_maze', function (req, res) {
+       res.json(maze);
+    });
+
+    setupSockets(app);
+}
+
+function setupSockets(app) {
+    let http = require('http').Server(app);
+    let io = require('socket.io');
+    let display_sock = io.of('/display');
+    let control_sock = io.of('/controller');
+
+    display_sock.on('connection', function(socket) {
+        console.log('new display ' + socket.id);
+    });
+
+    control_sock.on('connection', function(socket) {
+        console.log('new controller ' + socket.id);
+    })
+}
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function getEmptySpots(maze){
+    let emptySpots = [];
+
+    for(let i = 1; i < maze.length - 1; i++){
+        for(let j = 1; j < maze[0].length - 1; j++){
+            if(maze[i][j] === EMPTY)
+                emptySpots.push([i, j]);
+        }
+    }
+
+    shuffleArray(emptySpots);
+
+    return emptySpots;
+}
+
+/*
+let emptySpots = getEmptySpots(maze);
+let currSpot = 0;
+for(let key in players){
+    players[key] = {x: emptySpots[currSpot][0], y: emptySpots[currSpot][1], velX:0, velY:0, accX:0, accY:0};
+    currSpot++;
+}
+*/
+
+module.exports = setup;
