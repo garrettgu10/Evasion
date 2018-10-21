2849da76e734c45c137bb41e7ab7dc2822780929
Garrett Gu <garrettgu777@gmail.com>
2018-10-20 23:50:48 -0500

    added default "good" color

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ var md5 = require('md5');
 var maze = mazeGen(MAZE_SIZE, MAZE_SIZE);
 var emptySpots = getEmptySpots(maze);
 var players = {};
+var colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
 
 function setup(app) {
     app.use('/new_game', function(req, res) {
@@ -29,7 +39,11 @@ function setup(app) {
 }
 
 function getColor(id) {
-    return '#'+md5(id).substring(0, 6);
+    if(colorArray.length === 0){ //out of colors, use md5
+        return '#'+md5(id).substring(0, 6);
+    }else{
+        return colorArray.pop();
+    }
 }
 
 function setupSockets(server) {
